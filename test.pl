5484e42c82597da92f6c8d3488ca1c912c2d8b46
Jon Ingvar Skånøy <jiskaanoy@student.uia.no>
2023-11-11 17:16:22 +0100

    test

--- test.pl
+++ test.pl
@@ -12,45 +12,56 @@ add(A,B,A+B).
 readFile(FileName) :-
     setup_call_cleanup(
         open(FileName, read, Stream),
-        readLines(Stream),
+        readLines(Stream, not_in_list, [], 0, 0),
         close(Stream)
     ).
 
-readLines(Stream) :-
-    read_line_to_string(Stream, Line),
-    Line \= end_of_file,
-    processLine(Line),
-    readLines(Stream).
-readLines(_).
-
-% Process each line, check if it's a dimension string, and create a list if it is
-processLine(Line) :-
-    (is_dimension_string(Line) ->
-        extract_dimensions(Line, Dimensions),
-        create_list(Dimensions, List),
-        writeln(List)
+readLines(Stream, in_list, AccumulatedList, Width, Height) :-
+    length(AccumulatedList, CurrentHeight),
+    (CurrentHeight < Height ->
+        read_line_to_string(Stream, Line),
+        Line \= end_of_file,
+        processLine(Line, Stream, AccumulatedList, UpdatedList, Width),
+        readLines(Stream, in_list, UpdatedList, Width, Height)
     ;
-        writeln(Line)  % Else, print the line as is.
+        writeln(AccumulatedList),
+        (CurrentHeight =:= Height -> readLines(Stream, not_in_list, [], 0, 0) ; true)
+    ).
+readLines(Stream, not_in_list, _, _, _) :-
+    read_line_to_string(Stream, Line),
+    (Line == end_of_file -> true ;
+     (is_dimension_string(Line) ->
+        extract_dimensions(Line, Dimensions),
+        split_string(Dimensions, "x", "", [W, H]),
+        number_string(Width, W),
+        number_string(Height, H),
+        readLines(Stream, in_list, [], Width, Height)
+      ;
+        writeln(Line),
+        readLines(Stream, not_in_list, [], 0, 0)
+     )
     ).
 
-% Check if the line is a dimension string (e.g., "size 7x5")
+processLine(Line, _, AccumulatedList, UpdatedList, Width) :-
+    split_string(Line, " ", "", SplitLine),
+    maplist(atom_string, Atoms, SplitLine),
+    length(Atoms, LineWidth),
+    (LineWidth =:= Width ->
+        append(AccumulatedList, [Atoms], UpdatedList)
+    ;
+        writeln('Line does not match the expected width.'),
+        UpdatedList = AccumulatedList
+    ).
+
 is_dimension_string(Line) :-
     sub_string(Line, 0, _, _, "size ").
 
-% Extract dimensions from the line
 extract_dimensions(Line, Dimensions) :-
     sub_string(Line, _, _, 0, Dimensions).
 
-% Parses the dimension string and generates the list
-create_list(DimensionString, List) :-
-    split_string(DimensionString, "x", "", [W, H]),
-    number_string(Width, W),
-    number_string(Height, H),
-    generate_2d_list(Width, Height, List).
 
-% Generates a 2D list of Width x Height
-generate_2d_list(Width, Height, List) :-
-    findall(Row, (between(1, Height, _), length(Row, Width)), List).
+
+
 
 
 
